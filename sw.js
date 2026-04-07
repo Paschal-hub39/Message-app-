bca659bb72751f15e92ce5f05cd70a21249299fe
Paschal-hub39 <azubuikepaschal6@gmail.com>
2026-04-07 20:06:36 +0100

    Add basic Service Worker for Paschala Hub

---
+++ sw.js
@@ -0,0 +1,9 @@
+// This is a basic Service Worker for Paschala Hub
+self.addEventListener('install', (e) => {
+  console.log('SW Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Keeps the app running smoothly
+});
+
