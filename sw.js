f8c5bd064d06802ff92ca9ee37502f401cdf663d
Paschal-hub39 <azubuikepaschal6@gmail.com>
2026-04-08 09:09:45 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// This is a basic Service Worker for Paschala Hub
-self.addEventListener('install', (e) => {
-  console.log('SW Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Keeps the app running smoothly
-});
-
